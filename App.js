fbac0f63cf8a2940b1b06d1179e80c5f2e8900af
pauloh-fm <phfarmar@gmail.com>
2024-02-27 06:00:27 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import HomeScreen from './src/pages/HomeScreen';
+import AvaliacaoScreen from './src/pages/AvaliacaoScreen';
+import TelaAgradecimento from './src/pages/TelaAgradecimento';
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}  options={{headerShown: false}}/>
+        <Stack.Screen name="Avaliacao" component={AvaliacaoScreen} options={{headerShown: false}}/>
+				<Stack.Screen name="TelaAgradecimento" component={TelaAgradecimento} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
